191d59333010a2ec772131299e943f8e1527d040
Walter Moreira <walter@waltermoreira.net>
2013-01-23 15:35:43 -0600

    Add standard distutil files

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup
+
+setup(name='absence',
+      version='0.1.0',
+      description='Wrapper for day-to-day use of duplicity.',
+      author='Walter Moreira',
+      author_email='walter@waltermoreira.net',
+      packages=['absence'],
+      scripts=['bin/absence.py'],
+      license='LICENSE.txt',
+      long_description=open('README.rst').read(),
+      )
