1aa87368c58091d064e92d841afe342c6f00f778
Walter Moreira <walter@waltermoreira.net>
2013-07-24 11:31:18 -0500

    Add defaults to config parsing of secrets

--- absence/secrets.py
+++ absence/secrets.py
@@ -4,11 +4,19 @@ import ConfigParser
 DIRECTORY = '~/.absence'
 FILE = 'secrets'
 
+class ConfigParserWithDefaults(ConfigParser.ConfigParser):
+
+    def get(self, *args, **kwargs):
+        try:
+            ConfigParser.ConfigParser.get(self, *args, **kwargs)
+        except (ConfigParser.NoSectionError, ConfigParser.NoOptionError):
+            return None
+            
 def read():
     try:
         os.makedirs(os.path.expanduser(DIRECTORY))
     except OSError:
         pass
-    c = ConfigParser.ConfigParser()
+    c = ConfigParserWithDefaults()
     c.read(os.path.expanduser(os.path.join(DIRECTORY, FILE)))
     return c
